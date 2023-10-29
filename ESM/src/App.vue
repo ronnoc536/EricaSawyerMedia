e466a39b77accd0e0b8f4b98f1fd1eb2c5f8703b
Connor Marler <connormarler536@gmail.com>
2023-10-28 19:40:24 -0500

    please work

---
+++ ESM/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import Navbar from "./components/Navbar.vue";
+</script>
+
+<template>
+  <Navbar />
+  <div>Hello World</div>
+  <h1 class="text-3xl font-bold underline bg-red-200">
+    Hello world!
+  </h1>
+</template>
+
+<style scoped>
+
+</style>
+
